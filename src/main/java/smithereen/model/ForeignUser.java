932de2de076dca8c92351785e8ad26a40a7e2c16
Grishka <git@grishka.me>
2025-02-17 23:34:57 +0300

    Recognize "web site" as contact info key

--- src/main/java/smithereen/model/ForeignUser.java
+++ src/main/java/smithereen/model/ForeignUser.java
@@ -264,7 +264,7 @@ public class ForeignUser extends User implements ForeignActor{
 					// Get rid of Mastodon :emojis: and non-ASCII characters
 					String normalizedName=pv.name.toLowerCase().replaceAll(":[a-z0-9_]{2,}:", "").replaceAll("[^a-z0-9 -]", "").trim();
 					// Match against popular strings people use for these things
-					if(Set.of("website", "web", "blog", "homepage", "www", "site", "personal page", "personal website", "personal blog").contains(normalizedName)){
+					if(Set.of("website", "web", "web site", "blog", "homepage", "www", "site", "personal page", "personal website", "personal blog").contains(normalizedName)){
 						website=TextProcessor.stripHTML(pv.value, false);
 						continue;
 					}
