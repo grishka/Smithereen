aad0a44e3a996093cb3131ed4f155d14297491aa
Grishka <git@grishka.me>
2025-05-19 06:37:26 +0300

    Fix file structure

--- ci/prepare_bundles.sh
+++ ci/prepare_bundles.sh
@@ -3,8 +3,9 @@
 function doOneArch {
 	mkdir bundles/$1
 	cp -v target/smithereen.jar bundles/$1/
-	cp -v -R target/lib/ bundles/$1/lib/
-	cp -v -R imgproxy-libvips-$1 bundles/$1
+	cp -v -R target/lib bundles/$1/lib
+	cp -v imgproxy-libvips-$1/* bundles/$1/
+	chmod +x bundles/$1/imgproxy # permissions get lost in the process of artifact download
 	# TODO install script?
 }
 
