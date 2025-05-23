b3913932b92481ccdc75da7ef149b2f0d4f0ec86
Grishka <git@grishka.me>
2025-05-23 10:33:28 +0300

    Work around actions/upload-artifact#93

--- ci/prepare_bundles.sh
+++ ci/prepare_bundles.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 function doOneArch {
-	mkdir bundles/$1
-	cp -v -R target/smithereen.jar target/lib imgproxy-libvips-$1/* schema.sql examples/nginx.conf bundles/$1
-	chmod +x bundles/$1/imgproxy # permissions get lost in the process of artifact download
-	cp ci/bundle_install.sh bundles/$1/install.sh
-	sed -i -e "s/__arch__/$2/g" bundles/$1/install.sh
-	chmod +x bundles/$1/install.sh
-	pushd bundles/$1
+	mkdir bundles/$3
+	cp -v -R target/smithereen.jar target/lib imgproxy-libvips-$1/* schema.sql examples/nginx.conf bundles/$3
+	chmod +x bundles/$3/imgproxy # permissions get lost in the process of artifact download
+	cp ci/bundle_install.sh bundles/$3/install.sh
+	sed -i -e "s/__arch__/$2/g" bundles/$3/install.sh
+	chmod +x bundles/$3/install.sh
+	pushd bundles/$3
 	ln -s libvips-cpp.so.* libvips-cpp.so
 	popd
 }
 
 mkdir bundles
-doOneArch "amd64" "x86_64"
-doOneArch "arm64" "aarch64"
+doOneArch "amd64" "x86_64" "x86_64"
+doOneArch "arm64" "aarch64" "arm64"
+
+tar -cvzf bundles
