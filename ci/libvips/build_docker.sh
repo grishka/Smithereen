f0ef0e95eb0a70d1e86aaa18fe7f26e6462a997d
Grishka <git@grishka.me>
2025-09-17 13:23:26 +0300

    Check if ci/libvips_bin exists

--- ci/libvips/build_docker.sh
+++ ci/libvips/build_docker.sh
@@ -3,7 +3,7 @@
 # Note: Github actions build libvips as a separate step, to share the built binaries in the cache across all actions.
 # This wrapper script is only needed for when you're building a Docker image outside of CI.
 
-if [ -f ci/libvips_bin/libvips.so.* ]; then
+if [ -d "ci/libvips_bin" ]; then
 	echo "libvips already built"
 else
 	echo "Building libvips"
